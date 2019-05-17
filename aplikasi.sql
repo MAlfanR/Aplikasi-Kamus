5b0b774f46cd13136143198dc3720e0449e2b4b0
MAlfanR <45311203+MAlfanR@users.noreply.github.com>
2019-05-17 15:55:50 +0700

    database terbaru

--- aplikasi.sql
+++ aplikasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2019 at 08:57 AM
+-- Generation Time: May 17, 2019 at 02:59 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -47,21 +47,26 @@ INSERT INTO `admin` (`nama_admin`, `password`, `nip`, `jenis_kelamin`, `username
 -- --------------------------------------------------------
 
 --
--- Table structure for table `informatika`
+-- Table structure for table `kata`
 --
 
-CREATE TABLE `informatika` (
-  `id` int(4) NOT NULL,
-  `kata` varchar(50) NOT NULL,
-  `penjelasan_kata` text NOT NULL
+CREATE TABLE `kata` (
+  `keyword` varchar(25) NOT NULL,
+  `deskripsi_kata` text,
+  `kd_prodi` char(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `informatika`
+-- Dumping data for table `kata`
 --
 
-INSERT INTO `informatika` (`id`, `kata`, `penjelasan_kata`) VALUES
-(1, 'Java', 'Java adalah bahasa pemrograman yang dapat dijalankan di berbagai komputer termasuk telepon genggam. Bahasa ini awalnya dibuat oleh James Gosling saat masih bergabung di Sun Microsystems saat ini merupakan bagian dari Oracle dan dirilis tahun 1995.');
+INSERT INTO `kata` (`keyword`, `deskripsi_kata`, `kd_prodi`) VALUES
+('', '', 'IF'),
+('c++', 'bahasa pemrograman', 'IF'),
+('java', 'bahasa pemrograman', 'IF'),
+('Linux', 'Merupakan salah satu sistem operasi', 'IF'),
+('Phyton', 'interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python\'s design philosophy emphasizes code readability with its notable use of significant whitespace. ', 'IF'),
+('tumbuhan', 'sebuah tanaman yang', 'BI');
 
 -- --------------------------------------------------------
 
@@ -71,8 +76,7 @@ INSERT INTO `informatika` (`id`, `kata`, `penjelasan_kata`) VALUES
 
 CREATE TABLE `mahasiswa` (
   `nim` int(10) NOT NULL,
-  `first_name` varchar(15) DEFAULT NULL,
-  `last_name` varchar(15) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
   `tgl_lahir` char(10) DEFAULT NULL,
   `username` varchar(12) DEFAULT NULL,
   `password` varchar(12) DEFAULT NULL,
@@ -84,9 +88,29 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `first_name`, `last_name`, `tgl_lahir`, `username`, `password`, `prodi`, `angkatan`) VALUES
-(14117, 'Andi', 'Riyad', '30-11-1999', 'andiriyad', '123456', 'T.Informatika', 2018),
-(14117150, 'M.Alfan', 'Riyadi', '25-11-1999', 'ranger25', '123456', 'T.Informatika', 2017);
+INSERT INTO `mahasiswa` (`nim`, `name`, `tgl_lahir`, `username`, `password`, `prodi`, `angkatan`) VALUES
+(14117, 'Andi', '30-11-1999', 'andiriyad', '123456', 'T.Informatika', 2018),
+(14117150, 'M.Alfan', '25-11-1999', 'ranger25', '123456', 'T.Informatika', 2017);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prodi`
+--
+
+CREATE TABLE `prodi` (
+  `kd_prodi` char(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prodi`
+--
+
+INSERT INTO `prodi` (`kd_prodi`) VALUES
+('AR'),
+('BI'),
+('EL'),
+('IF');
 
 --
 -- Indexes for dumped tables
@@ -99,16 +123,33 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`nip`);
 
 --
--- Indexes for table `informatika`
+-- Indexes for table `kata`
 --
-ALTER TABLE `informatika`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `kata`
+  ADD PRIMARY KEY (`keyword`),
+  ADD KEY `kd_prodi` (`kd_prodi`);
 
 --
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `prodi`
+--
+ALTER TABLE `prodi`
+  ADD PRIMARY KEY (`kd_prodi`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kata`
+--
+ALTER TABLE `kata`
+  ADD CONSTRAINT `kata_ibfk_1` FOREIGN KEY (`kd_prodi`) REFERENCES `prodi` (`kd_prodi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
